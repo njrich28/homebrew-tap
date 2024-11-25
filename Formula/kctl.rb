b402b6880d79d0d9f3656b76daaed710faac418f
Neil Richardson <960806+njrich28@users.noreply.github.com>
2024-11-25 21:02:52 +0000

    Update kctl.rb to v0.1.1

--- Formula/kctl.rb
+++ Formula/kctl.rb
@@ -1,20 +1,20 @@
 class Kctl < Formula
   desc "A very special CLI"
   homepage "https://neilrichardson.online"
-  url "https://neilrichardson.online/versions/0.1.0/7c704e8/kctl-v0.1.0-7c704e8-darwin-arm64.tar.xz"
-  sha256 "6b86d75f97fc7668ecab60e195a973757523f53cc2086d541aa210fbc97babac"
-  version "0.1.0"
+  url "https://neilrichardson.online/versions/0.1.1/1c7fabd/kctl-v0.1.1-1c7fabd-darwin-arm64.tar.xz"
+  sha256 "07a456975cd13b468982487de0c3e5d2b68bd20b6999ff443f1cc6fb65d55b28"
+  version "0.1.1"
   version_scheme 1
 
   on_macos do
     on_intel do
-      url "https://neilrichardson.online/versions/0.1.0/7c704e8/kctl-v0.1.0-7c704e8-darwin-x64.tar.xz"
-      sha256 "203bf4f26ebec312bdb924b0665a4ea85e39dbacafd3f4462dbb1cd8b49e5dbc"
+      url "https://neilrichardson.online/versions/0.1.1/1c7fabd/kctl-v0.1.1-1c7fabd-darwin-x64.tar.xz"
+      sha256 "1fc2e4d8ac42cfeb97eb98f047967f597e0abfab81ca18a88c5cac2800c01ed5"
     end
   end
 
   def install
-    # inreplace "bin/kctl", "/^CLIENT_HOME=/", "export KCTL_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/kctl", /^CLIENT_HOME=/, "export KCTL_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/kctl"
   end
